2f337e70560c372fd746cad35cda102890090942
Supremoestelar369 <111463579+Supremoestelar369@users.noreply.github.com>
2022-10-17 20:38:02 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
 
          </div>
          <section className="inf-userName flex">
-            <h2 className='inf-userName__h2'>{`Hello pokemon Master ${userName}`}</h2>
+            <h2 className='inf-userName__h2'>{`Hello Welcome${userName}`}</h2>
             <h3 className='inf-userName__p-big-font'>Here are your pokemon, search and enjoy</h3>
          </section>
 
